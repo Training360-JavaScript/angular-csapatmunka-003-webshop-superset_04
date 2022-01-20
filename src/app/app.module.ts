c5a84398f6dd5be9cb8535ec28c9da0968417fe0
klvinbagoly <kbalazsk1@gmail.com>
2022-01-20 23:05:07 +0100

    route correction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { Cat01Component } from './page/cat01/cat01.component';
 import { Cat02Component } from './page/cat02/cat02.component';
 import { HomeComponent } from './page/home/home.component';
 import { ProductPagerComponent } from './common/product-pager/product-pager.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './common/product-list/product-list.component';
 import { FilterPipe } from './pipe/filter.pipe';
 
 @NgModule({
