21da5dcdbf7d64f414acc9caa8e940a2fd2c141e
Kocsis Gábor <ifjkocsis.gabor@gmail.com>
2022-01-22 20:58:21 +0100

    4.

--- src/app/common/product-card/product-card.component.ts
+++ src/app/common/product-card/product-card.component.ts
@@ -8,7 +8,8 @@ import { Product } from '../../model/product';
 })
 export class ProductCardComponent implements OnInit {
 
-  @Input() productList: Product[];
+  @Input()
+  productList: Product[] = [];
 
   constructor() { }
 
