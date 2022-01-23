19c1d2b63e5373c839b27df947bb135252b6d9ea
Cserhati Imre Andras <cserhati.imre.andras@gmail.com>
2022-01-23 04:13:13 +0100

    home v1

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,15 +1,43 @@
+import { ProductService } from './../../service/product.service';
+import { Product } from './../../model/product';
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  products: Product[];
+  featuredProducts: Product[];
+  randomProducts: Product[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private productService: ProductService) {
+    this.products = this.productService.getAll();
+    this.featuredProducts = this.selectFeaturedProducts(5);
+    this.randomProducts = this.selectRandomProducts(5);
   }
 
+  ngOnInit(): void {}
+
+  selectFeaturedProducts(n: number): Product[] {
+    return this.products.filter((product) => product.featured).slice(0, n);
+  }
+
+  selectRandomProducts(n: number): Product[] {
+    let noneFeaturedProducts = this.products.filter(
+      (product) => !product.featured
+    );
+    return this.getRandomItems(noneFeaturedProducts, 5);
+  }
+
+  getRandomItems(arr: any[], n: number): any[] {
+    let randomSet = new Set();
+    while (randomSet.size < 5) {
+      let index = Math.floor(arr.length * Math.random());
+      randomSet.add(arr[index]);
+    }
+    return [...randomSet];
+  }
 }
