416b6f738df1b599c336b3c8e52e79f6406f00aa
hvroberta89 <veberroberta@gmail.com>
2022-02-02 20:53:45 +0100

    url product service

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -28,11 +28,11 @@ export class ProductService {
   }
 
   update(product: Product): Observable<any> {
-    return this.http.put( `${environment.jsonApiUrl}/${this.jsonName}/${product.id}`, product )
+    return this.http.put( `${this.url}/${product.id}`, product )
   }
 
   remove(product: any): Observable<any> {
     product = product.id ? product : product;
-    return this.http.delete( `${environment.jsonApiUrl}/${this.jsonName}/${product}` );
+    return this.http.delete( `${this.url}/${product}` );
   }
 }
