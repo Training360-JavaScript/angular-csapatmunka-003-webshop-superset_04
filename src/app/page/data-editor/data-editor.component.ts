15f0fa79539415f35561c7d9d47535e89f635d4c
hvroberta89 <veberroberta@gmail.com>
2022-02-03 00:18:03 +0100

    sorter admin page

--- src/app/page/data-editor/data-editor.component.ts
+++ src/app/page/data-editor/data-editor.component.ts
@@ -14,6 +14,7 @@ export class DataEditorComponent implements OnInit {
   keys!: string[];
   searchKey!: string;
   searchPhrase!: string;
+  sortKey!: string;
 
   constructor( private productService: ProductService) {}
 
